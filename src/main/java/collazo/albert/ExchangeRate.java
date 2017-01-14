db97c3ce50398c0ac95f579dff81495b05207532
Albert Collazo <albertcollazo@zipcoders-MacBook-Pro-8.local>
2017-01-13 21:39:45 -0500

    Second Commit

--- src/main/java/collazo/albert/ExchangeRate.java
+++ src/main/java/collazo/albert/ExchangeRate.java
@@ -3,129 +3,121 @@ package collazo.albert;
 
 public class ExchangeRate {
 
-        double usd =1;
-        double euro = 0.94;
-        double bp = 0.82;
-        double ir = 68.32;
-        double ad = 1.35;
-        double cd = 1.32;
-        double sd = 1.43;
-        double sf = 1.01;
-        double mr = 4.47;
-        double jy = 115.84;
-        double cy = 6.92;
+    double usd = 1;
+    double euro = 0.94;
+    double bp = 0.82;
+    double ir = 68.32;
+    double cd = 1.32;
+    double sd = 1.43;
+    double sf = 1.01;
+    double mr = 4.47;
+    double jy = 115.84;
+    double cy = 6.92;
 
-    public String usdToEuro(){
+    public String usdToEuro() {
 
-        double d = euro/usd;
-
-        String answer = String.format("Exchange is %.2f", d);
-
-        return answer;
-    }
-
-    public String euroToUsd(){
-
-        double d = usd/euro;
+        double d = euro / usd;
 
         String answer = String.format("Exchange is %.2f", d);
 
         return answer;
     }
 
-    public String euroToBp(){
+    public String euroToUsd() {
 
-        double a = usd/euro;
-        double b = usd/bp;
-        double d = a/b;
+        double d = usd / euro;
 
         String answer = String.format("Exchange is %.2f", d);
 
         return answer;
     }
 
-    public String bpToIr(){
+    public String euroToBp() {
 
-        double a = usd/bp;
-        double b = usd/ir;
-        double d = a/b;
-
-        String answer = String.format("Exchange is %.2f", d);
-
-        return answer;
-    }
-
-    public String irToCd(){
-
-        double a = usd/ir;
-        double b = usd/cd;
-        double d = a/b;
+        double a = usd / euro;
+        double b = usd / bp;
+        double d = a / b;
 
         String answer = String.format("Exchange is %.2f", d);
 
         return answer;
     }
 
-    public String cdToSd(){
+    public String bpToIr() {
 
-        double a = usd/cd;
-        double b = usd/sd;
-        double d = a/b;
-
-        String answer = String.format("Exchange is %.2f", d);
-
-        return answer;
-    }
-
-    public String sdToSf(){
-
-        double a = usd/sd;
-        double b = usd/sf;
-        double d = a/b;
+        double a = usd / bp;
+        double b = usd / ir;
+        double d = a / b;
 
         String answer = String.format("Exchange is %.2f", d);
 
         return answer;
     }
 
-    public String sfToMr(){
+    public String irToCd() {
 
-        double a = usd/sf;
-        double b = usd/mr;
-        double d = a/b;
-
-        String answer = String.format("Exchange is %.2f", d);
-
-        return answer;
-    }
-
-    public String mrToJy(){
-
-        double a = usd/mr;
-        double b = usd/jy;
-        double d = a/b;
+        double a = usd / ir;
+        double b = usd / cd;
+        double d = a / b;
 
         String answer = String.format("Exchange is %.2f", d);
 
         return answer;
     }
 
-    public String jyToCy(){
+    public String cdToSd() {
 
-        double a = usd/jy;
-        double b = usd/cy;
-        double d = a/b;
+        double a = usd / cd;
+        double b = usd / sd;
+        double d = a / b;
 
         String answer = String.format("Exchange is %.2f", d);
 
         return answer;
     }
 
-    public static void main(String[] args){
+    public String sdToSf() {
 
+        double a = usd / sd;
+        double b = usd / sf;
+        double d = a / b;
 
+        String answer = String.format("Exchange is %.2f", d);
+
+        return answer;
     }
 
+    public String sfToMr() {
 
+        double a = usd / sf;
+        double b = usd / mr;
+        double d = a / b;
 
+        String answer = String.format("Exchange is %.2f", d);
+
+        return answer;
     }
+
+    public String mrToJy() {
+
+        double a = usd / mr;
+        double b = usd / jy;
+        double d = a / b;
+
+        String answer = String.format("Exchange is %.2f", d);
+
+        return answer;
+    }
+
+    public String jyToCy() {
+
+        double a = usd / jy;
+        double b = usd / cy;
+        double d = a / b;
+
+        String answer = String.format("Exchange is %.2f", d);
+
+        return answer;
+    }
+
+}
---
+++ src/main/java/collazo/albert/Main.java
@@ -0,0 +1,15 @@
+package collazo.albert;
+
+
+public class Main {
+
+    public static void main(String[] args){
+        ExchangeRate wuTang = new ExchangeRate();
+        wuTang.usdToEuro();
+
+    }
+
+
+
+}
+
